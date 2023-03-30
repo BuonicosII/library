a07d76ae423344555c735a045b1dbf104be655ca
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-03-30 20:03:34 +0200

    Add eventlistener to form to run addToLibrary

--- script.js
+++ script.js
@@ -6,13 +6,20 @@ function Book(title, author, read) {
     this.read = read
 };
 
-function addBookToLibrary(title, author, read) {
+function addBookToLibrary(event) {
     //azione da collegare al submit del form e.click
+    event.preventDefault();
     //contestualmente al submit del form viene creato un nuovo oggetto libro
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let read = document.querySelector('#readstatus').value;
     let newBook = new Book(title, author, read);
     //oggetto viene pushato nell'array
-    myLibrary.push(newBook)
+    myLibrary.push(newBook);
     //funzione per creare le card in pagina manipolando il dom
+    
   }
 
-  const Hobbit = new Book('The Hobbit', 'Tolkien', 'Read')
+const submitButton = document.querySelector('#submitbutton');
+submitButton.addEventListener("click", addBookToLibrary);
+  //const Hobbit = new Book('The Hobbit', 'Tolkien', 'Read')
