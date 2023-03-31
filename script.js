58c799891bed50e4cd8e0786f2a39f0cc578e7c0
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-03-31 22:57:09 +0200

    AddBookToLibrary now creates card in shelf div

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let myLibrary = [];
+let shelf = document.querySelector('#shelf');
 
 function Book(title, author, read) {
     this.title = title
@@ -7,19 +8,49 @@ function Book(title, author, read) {
 };
 
 function addBookToLibrary(event) {
-    //azione da collegare al submit del form e.click
+
+    //previene l'azione default di un submit button
+
     event.preventDefault();
+
     //contestualmente al submit del form viene creato un nuovo oggetto libro
+
     let title = document.querySelector('#title').value;
     let author = document.querySelector('#author').value;
     let read = document.querySelector('#readstatus').value;
     let newBook = new Book(title, author, read);
-    //oggetto viene pushato nell'array
+
+    //oggetto viene pushato nell'array myLibrary
+
     myLibrary.push(newBook);
+
     //funzione per creare le card in pagina manipolando il dom
+
+    //rimuove tutte le card attualmente presenti sullo shelf evitando doppioni
+
+    while (shelf.hasChildNodes()) {
+      shelf.removeChild(shelf.firstChild);
+    };
     
+    //loop che crea per ogni item dell'array una card con le informazioni estratte
+    //dall'oggetto
+
+    for (const book of myLibrary) {
+      let card = document.createElement('div');
+      let title = document.createElement('h2');
+      let author = document.createElement('h3');
+      let read = document.createElement('p');
+      title.textContent = book.title; 
+      author.textContent = book.author;
+      read.textContent = book.read;
+      shelf.appendChild(card);
+      card.appendChild(title);
+      card.appendChild(author);
+      card.appendChild(read);
+    }
   }
 
 const submitButton = document.querySelector('#submitbutton');
 submitButton.addEventListener("click", addBookToLibrary);
-  //const Hobbit = new Book('The Hobbit', 'Tolkien', 'Read')
+
+
