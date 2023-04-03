4ddcf23fbbb93cc50a7f7830b61a3001586aec18
BuonicosII <118397101+BuonicosII@users.noreply.github.com>
2023-04-03 19:34:06 +0200

    Refactor addToLibrary and removeFromLibrary code
    Refactor addToLibrary and removeFromLibrary functions' code creating a
    orderShelf function to handle the DOM manipulation

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 let myLibrary = [];
 let shelf = document.querySelector('#shelf');
 
+//object constructor libro
+
 function Book(title, author, read) {
     this.title = title
     this.author = author
     this.read = read
 };
 
-//funzione per rimuovere libro dall'array myLibrary e dallo shelf
+//funzione per rimuovere tutti gli elementi dom e crearne nuovi in base agli oggetti
+//presenti nell'array myLibrary
 
-function removeFromLibrary(event) {
-  myLibrary.splice(event.target.getAttribute("data-index"), 1);
+function orderShelf() {
+
+//rimuove tutte le card attualmente presenti sullo shelf evitando doppioni
 
   while (shelf.hasChildNodes()) {
     shelf.removeChild(shelf.firstChild);
   };
+
+//loop che crea per ogni item dell'array una card con le informazioni estratte
+//dall'oggetto
 
   for (const book of myLibrary) {
     let card = document.createElement('div');
@@ -31,10 +38,18 @@ function removeFromLibrary(event) {
     card.appendChild(author);
     card.appendChild(read);
     card.appendChild(removeButton);
-    card.setAttribute('data-index', `${myLibrary.findIndex(function (item) {return item.title === book.title})}`);
     removeButton.setAttribute('data-index', `${myLibrary.findIndex(function (item) {return item.title === book.title})}`);
     removeButton.addEventListener('click', removeFromLibrary);
   }
+
+}
+
+//funzione per rimuovere libro dall'array myLibrary e dallo shelf
+
+function removeFromLibrary(event) {
+  myLibrary.splice(event.target.getAttribute("data-index"), 1);
+
+  orderShelf();
 };
 
 //funzione per aggiungere un libro alla libreria
@@ -56,36 +71,7 @@ function addBookToLibrary(event) {
 
     myLibrary.push(newBook);
 
-    //funzione per creare le card in pagina manipolando il dom
-
-    //rimuove tutte le card attualmente presenti sullo shelf evitando doppioni
-
-    while (shelf.hasChildNodes()) {
-      shelf.removeChild(shelf.firstChild);
-    };
-    
-    //loop che crea per ogni item dell'array una card con le informazioni estratte
-    //dall'oggetto
-
-    for (const book of myLibrary) {
-      let card = document.createElement('div');
-      let title = document.createElement('h2');
-      let author = document.createElement('h3');
-      let read = document.createElement('p');
-      let removeButton = document.createElement('button');
-      title.textContent = book.title; 
-      author.textContent = book.author;
-      read.textContent = book.read;
-      removeButton.textContent = 'Remove';
-      shelf.appendChild(card);
-      card.appendChild(title);
-      card.appendChild(author);
-      card.appendChild(read);
-      card.appendChild(removeButton);
-      card.setAttribute('data-index', `${myLibrary.findIndex(function (item) {return item.title === book.title})}`);
-      removeButton.setAttribute('data-index', `${myLibrary.findIndex(function (item) {return item.title === book.title})}`);
-      removeButton.addEventListener('click', removeFromLibrary);
-    }
+    orderShelf();
   }
 
 const submitButton = document.querySelector('#submitbutton');
